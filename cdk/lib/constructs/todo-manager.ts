a50b684c19e12db306a32a28c838fd733e5f5e14
pabcrudel <pabcrudel@gmail.com>
2023-07-30 18:10:48 +0200

    Create 4 Lambdas and Grant DynamoDB Permissions

--- cdk/lib/constructs/todo-manager.ts
+++ cdk/lib/constructs/todo-manager.ts
@@ -28,5 +28,46 @@ export class TodoManagerConstruct extends cdk.Stack {
             encryption: dynamodb.TableEncryption.AWS_MANAGED,
             removalPolicy: cdk.RemovalPolicy.DESTROY,
         });
+
+        /**
+         * Array containing the names of Lambda functions to be created.
+         * These function names will be used in the loop to create the respective Lambda functions.
+         */
+        const lambdaFunctionNames = [
+            "GetToDosFunction",
+            "GetToDoFunction",
+            "PutToDoFunction",
+            "DeleteToDosFunction",
+        ];
+
+        const lambdaFunctions = lambdaFunctionNames.map((lambdaFunctionName) => {
+            /** Function name in hyphen-separated lowercase letters. */
+            const formatedFunctionName = lambdaFunctionName.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+
+            /**
+            * Common configuration settings for the 4 Lambda functions being created, including:
+            * 
+            * - Handler: The entry point function that Lambda executes.
+            * - Runtime: The Node.js 16.x runtime for the Lambda functions.
+            * - Environment: Environment variables to be passed to the Lambda functions.
+            *   - TABLE_NAME: The name of the DynamoDB table (`todoTable`) used by the functions.
+            */
+            const lambdaFunction = new lambdaNode.NodejsFunction(this, lambdaFunctionName, {
+                entry: `./lambda-functions/${formatedFunctionName}.ts`,
+                handler: "handler",
+                runtime: lambda.Runtime.NODEJS_16_X,
+                environment: {
+                    TABLE_NAME: todoTable.tableName,
+                },
+            });
+
+            // Grant appropriate permissions to the Lambda function over the DynamoDB table.
+            // - If the function name includes "get", grant read access using 'grantReadData'.
+            // - Otherwise, grant write access using 'grantWriteData'.
+            if (formatedFunctionName.includes("get")) todoTable.grantReadData(lambdaFunction);
+            else todoTable.grantWriteData(lambdaFunction);
+
+            return lambdaFunction;
+        });
     };
 };
