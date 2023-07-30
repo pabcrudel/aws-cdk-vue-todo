db51871ae5858b5480c517f260af700c750c4fb2
pabcrudel <pabcrudel@gmail.com>
2023-07-30 13:23:40 +0200

    Created class signature and constructor

--- cdk/lib/constructs/website-deployment.ts
+++ cdk/lib/constructs/website-deployment.ts
@@ -5,3 +5,9 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+
+export class WebsiteDeploymentConstruct extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+    };
+};
