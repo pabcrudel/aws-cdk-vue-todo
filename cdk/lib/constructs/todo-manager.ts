e6d27646cdc629b77dbec6104edb00a037602071
pabcrudel <pabcrudel@gmail.com>
2023-07-30 17:12:17 +0200

    Created a ToDo dynamoDB Table

--- cdk/lib/constructs/todo-manager.ts
+++ cdk/lib/constructs/todo-manager.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class TodoManagerConstruct extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const todoTable = new dynamodb.Table(this, 'ToDo', {
+            partitionKey: {
+                name: 'name',
+                type: dynamodb.AttributeType.STRING
+            },
+            sortKey: {
+                name: 'id',
+                type: dynamodb.AttributeType.STRING
+            },
+            encryption: dynamodb.TableEncryption.AWS_MANAGED,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
     };
 };
