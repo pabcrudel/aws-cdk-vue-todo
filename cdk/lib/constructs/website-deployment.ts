6ea06581560d8438d27df10f321e06a3ad300131
pabcrudel <pabcrudel@gmail.com>
2023-07-30 13:25:08 +0200

    Created the s3 bucket where the web will be hosted

--- cdk/lib/constructs/website-deployment.ts
+++ cdk/lib/constructs/website-deployment.ts
@@ -9,5 +9,16 @@ import { Construct } from 'constructs';
 export class WebsiteDeploymentConstruct extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
+
+      /** The s3 bucket where the website will be hosted */
+      const s3HostingBucket = new s3.Bucket(this, 'S3HostingBucket', {
+        publicReadAccess: false,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        accessControl: s3.BucketAccessControl.PRIVATE,
+        objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+      });
     };
 };
