757d4ed36fe54906ab5dbf9e6422a8354580c4e5
pabcrudel <pabcrudel@gmail.com>
2023-07-30 13:12:49 +0200

    imported the necessary libraries

---
+++ cdk/lib/constructs/website-deployment.ts
@@ -0,0 +1,7 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
