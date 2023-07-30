d9fb92ad67dbabc15029ef829cca6f1a92ee5376
pabcrudel <pabcrudel@gmail.com>
2023-07-30 13:59:17 +0200

    feat: Create WebsiteDeploymentConstruct with CloudFront Security Headers
    - Created WebsiteDeploymentConstruct class for hosting a website using CloudFront.
    - Added CloudFront Origin Access Identity (OAI) user for better security.
    - Set up CloudFront Response Headers Policy with security headers for enhanced protection.
    - Deployed frontend files to S3 bucket for website hosting.

--- cdk/lib/vue-todo-stack.ts
+++ cdk/lib/vue-todo-stack.ts
@@ -1,8 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { WebsiteDeploymentConstruct } from './constructs/website-deployment';
 
 export class VueToDoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Deployment infrastructure
+    const websiteDeployment = new WebsiteDeploymentConstruct(this, "WebsiteDeployment");
+
+    // Displays Website domain name on CloudFormation output
+    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
+      value: websiteDeployment.domainName,
+      description: 'Domain name of the CloudFront distribution',
+      exportName: "WebsiteDomainName"
+    });
   }
 }
