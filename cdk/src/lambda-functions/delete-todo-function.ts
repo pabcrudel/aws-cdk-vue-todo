2af35cbacfec0cc1f859971e5e946b0a48247315
pabcrudel <pabcrudel@gmail.com>
2023-07-31 19:05:51 +0200

    Created Delete ToDo lambda function

---
+++ cdk/src/lambda-functions/delete-todo-function.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoDBStore } from "../dynamodb-store";
+import { ToDoDynamoDB } from "../todo-interfaces";
+
+const dbStore: ToDoDynamoDB = new DynamoDBStore();
+
+exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let statusCode: number;
+    let body: string;
+
+    const id = event.pathParameters!.id;
+    if (id === undefined) {
+        statusCode = 400;
+        body = JSON.stringify({ message: "Missing 'id' parameter in path" });
+    }
+    else {
+        try {
+            const result = await dbStore.deleteToDo(id);
+
+            if (result === undefined) {
+                statusCode = 404;
+                body = JSON.stringify({ message: "ToDo not found" });
+            }
+            else {
+                statusCode = 200;
+                body = JSON.stringify({ message: "Product deleted" });
+            };
+        }
+        catch (error) {
+            statusCode = 500;
+            body = JSON.stringify(error);
+        };
+    };
+
+    return {
+        statusCode,
+        headers: { "content-type": "application/json" },
+        body
+    };
+};
