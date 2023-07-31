19c5be7dac47db735f761342f358dfed78314bbe
pabcrudel <pabcrudel@gmail.com>
2023-07-31 17:49:02 +0200

    Created getTodo function

--- cdk/src/dynamodb-store.ts
+++ cdk/src/dynamodb-store.ts
@@ -12,9 +12,17 @@ import {
 export class DynamoDBStore implements ToDoDynamo {
     private static tableName = process.env.TABLE_NAME;
     private static ddbClient: DynamoDBClient = new DynamoDBClient({});
-    private static ddbDocClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(this.ddbClient);
+    private static ddbDocClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(DynamoDBStore.ddbClient);
 
-    getToDo: (id: string) => Promise<ToDo | undefined>;
+    public async getToDo(id: string): Promise<ToDo | undefined> {
+        const params: GetCommand = new GetCommand({
+            TableName: DynamoDBStore.tableName,
+            Key: {id: id,},
+        });
+        const result:GetCommandOutput = await DynamoDBStore.ddbDocClient.send(params);
+        return result.Item as ToDo;
+    };
+
     putToDo: (todo: ToDo) => Promise<void>;
     deleteToDo: (id: string) => Promise<void>;
     getToDos: () => Promise<ToDo[] | undefined>;
