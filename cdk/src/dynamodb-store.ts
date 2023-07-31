b3a468bd26df0f25bff82dc798c8e99787566c6b
pabcrudel <pabcrudel@gmail.com>
2023-07-31 17:41:46 +0200

    Created base attributes

--- cdk/src/dynamodb-store.ts
+++ cdk/src/dynamodb-store.ts
@@ -1,12 +1,21 @@
 import { ToDo, ToDoDynamo } from './todo-interfaces';
-const { DynamoDB, Lambda } = require('aws-sdk');
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  GetCommandOutput,
+  PutCommand,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 export class DynamoDBStore implements ToDoDynamo {
     private static tableName = process.env.TABLE_NAME;
+    private static ddbClient: DynamoDBClient = new DynamoDBClient({});
+    private static ddbDocClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(this.ddbClient);
 
     getToDo: (id: string) => Promise<ToDo | undefined>;
     putToDo: (todo: ToDo) => Promise<void>;
     deleteToDo: (id: string) => Promise<void>;
     getToDos: () => Promise<ToDo[] | undefined>;
-    
 }
