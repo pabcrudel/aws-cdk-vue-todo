bf9c01127e41e59dce6f50ce1caba551734a2b7c
pabcrudel <pabcrudel@gmail.com>
2023-07-31 18:25:30 +0200

    Create Get ToDos lambda function

--- cdk/src/lambda-functions/get-todos-function.ts
+++ cdk/src/lambda-functions/get-todos-function.ts
@@ -1,2 +1,28 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyResult } from "aws-lambda";
 import { DynamoDBStore } from "../dynamodb-store";
+import { ToDoDynamoDB } from "../todo-interfaces";
+
+const dbStore: ToDoDynamoDB = new DynamoDBStore();
+
+exports.handler = async (): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+
+    try {
+        const result = await dbStore.getToDos();
+
+        response = {
+            statusCode: 200,
+            headers: { "content-type": "application/json" },
+            body: `{"ToDo": ${JSON.stringify(result)}}`,
+        }
+    }
+    catch (error) {
+        response = {
+            statusCode: 500,
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(error),
+        };
+    }
+
+    return response;
+};
