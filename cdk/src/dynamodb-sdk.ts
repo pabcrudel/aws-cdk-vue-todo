8fbcd2c2cfb8e33b13c738b280418bb150b320b8
pabcrudel <pabcrudel@gmail.com>
2023-08-02 01:15:29 +0200

    Moved functions to the api helper

--- cdk/src/dynamodb-sdk.ts
+++ cdk/src/dynamodb-sdk.ts
@@ -121,14 +121,4 @@ export class DynamodbSDK {
         };
         return parsedData;
     };
-
-    public validateUUID(uuid: string): boolean {
-        const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-        return uuidRegex.test(uuid);
-    };
-
-    public validateDate(dateStr: string): boolean {
-        const date = new Date(dateStr);
-        return !isNaN(date.getTime());
-    };
 };
