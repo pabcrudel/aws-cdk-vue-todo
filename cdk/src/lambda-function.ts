b6ddca7188d7a0093bdcdbee7beb496989d6f9a7
pabcrudel <pabcrudel@gmail.com>
2023-08-04 01:43:33 +0200

    Created classes to manage request errors and responses

--- cdk/src/lambda-function.ts
+++ cdk/src/lambda-function.ts
@@ -2,3 +2,47 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import * as dbSDK from "./dynamodb-sdk";
 import { randomUUID } from "crypto";
 
+class ApiError extends Error {
+    statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = "ApiError";
+    };
+};
+class BadRequestError extends ApiError {
+    constructor(message: string) {
+        super(message, 400);
+    };
+};
+class NotFoundError extends ApiError {
+    constructor(message: string) {
+        super(message, 404);
+    };
+};
+class ApiResponse implements APIGatewayProxyResult {
+    readonly statusCode: number;
+    readonly headers = { "content-type": "application/json" };
+    readonly body: string;
+
+    constructor(statusCode: number, rawBody: any) {
+        this.statusCode = statusCode;
+        this.body = JSON.stringify(rawBody);
+    };
+};
+class ApiSuccessResponse extends ApiResponse {
+    constructor(rawBody: any) {
+        super(200, rawBody);
+    };
+};
+class ApiErrorResponse extends ApiResponse {
+    constructor(error: any) {
+        super(
+            error instanceof ApiError ? error.statusCode : 500,
+            { error: error instanceof Error ? error.message : "Unknown error occurred" },
+        );
+    };
+};
+
+
