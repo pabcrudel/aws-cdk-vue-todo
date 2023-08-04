6d5fb7cd1ec4c2810522b5b34881ca0de5549622
pabcrudel <pabcrudel@gmail.com>
2023-08-04 11:19:48 +0200

    Changed child resource path name

--- cdk/lib/constructs/todo-manager.ts
+++ cdk/lib/constructs/todo-manager.ts
@@ -57,7 +57,7 @@ export class ToDoManagerConstruct extends Construct {
         todoRestApi.root.addMethod("DELETE", new apigw.LambdaIntegration(deleteToDo));
         
         // Add a "GET" method to the child resource using the specified Lambda integration.
-        const todoRestApiChildResource = todoRestApi.root.addResource('todo');
+        const todoRestApiChildResource = todoRestApi.root.addResource('filter');
         todoRestApiChildResource.addMethod("GET", new apigw.LambdaIntegration(getToDo));
     };
 
