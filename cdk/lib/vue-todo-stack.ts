52eaa7212e8e3e674582726a534e8e74df1f3777
pabcrudel <pabcrudel@gmail.com>
2023-08-04 20:25:02 +0200

    Store the Api url to use it in the frontend

--- cdk/lib/vue-todo-stack.ts
+++ cdk/lib/vue-todo-stack.ts
@@ -2,15 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { WebsiteDeploymentConstruct } from './constructs/website-deployment';
 import { ToDoManagerConstruct } from './constructs/todo-manager';
+import { S3ObjectStorage } from './constructs/s3-object-storage';
 
 export class VueToDoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Deployment infrastructure
-    new WebsiteDeploymentConstruct(this, "WebsiteDeployment");
+    const websiteDeployment = new WebsiteDeploymentConstruct(this, "WebsiteDeployment");
 
     // ToDo infrastructure
-    new ToDoManagerConstruct(this, "ToDoManager");
+    const toDoManager = new ToDoManagerConstruct(this, "ToDoManager");
+
+    // Store the Api url to use it in the frontend
+    new S3ObjectStorage(this, "apiConnectionInfoStorage", {
+      bucket: websiteDeployment.s3HostingBucket,
+      key: 'config.json', // SHOULD NOT BE CHANGED
+      object: {
+        apiUrl: toDoManager.apiUrl
+      },
+    });
   };
 };
