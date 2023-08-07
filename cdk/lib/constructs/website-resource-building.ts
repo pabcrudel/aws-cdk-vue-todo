2fdc04c8484090c3ced6e0477e07bddd1d15a07a
pabcrudel <pabcrudel@gmail.com>
2023-08-08 01:27:25 +0200

    Created an invalidation cache for cloudfront

--- cdk/lib/constructs/website-resource-building.ts
+++ cdk/lib/constructs/website-resource-building.ts
@@ -3,6 +3,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class WebsiteResourceBuildingConstruct extends Construct {
@@ -29,6 +32,21 @@ export class WebsiteResourceBuildingConstruct extends Construct {
             exportName: "S3BucketName"
         });
 
+        /** An AWS CloudWatch Events rule that triggers when there are changes made to the S3 bucket. */
+        const s3EventRule = new events.Rule(this, 'S3EventRule', {
+            eventPattern: {
+                source: ['aws.s3'],
+                detailType: ['AWS API Call via CloudTrail'],
+                detail: {
+                    eventSource: ['s3.amazonaws.com'],
+                    eventName: ['PutObject', 'CompleteMultipartUpload'], // Detecta cambios de carga
+                    requestParameters: {
+                        bucketName: [this.s3HostingBucket.bucketName],
+                    },
+                },
+            },
+        });
+
         /** CloudFront Origin Access Identity (OAI) user */
         const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
             this, 'CloudFrontOriginAccessIdentity'
@@ -41,17 +59,7 @@ export class WebsiteResourceBuildingConstruct extends Construct {
             principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
         }));
 
-        /**
-        * Creates a Cloudfront Response Headers Policy which applies security headers to enhance security.
-        * The security headers behavior includes the following:
-        * 
-        * - Content Security Policy
-        * - Strict Transport Security
-        * - X-Content-Type-Options
-        * - Referrer Policy
-        * - XSS Protection
-        * - Frame Options
-        */
+        /** Creates a Cloudfront Response Headers Policy which applies security headers to enhance security. */
         const responseHeaderPolicy = new cloudfront.ResponseHeadersPolicy(this, 'CloudFrontResponseHeaderPolicy', {
             comment: 'Security headers response header policy',
             securityHeadersBehavior: {
@@ -105,6 +113,34 @@ export class WebsiteResourceBuildingConstruct extends Construct {
                 }
             ]
         });
+
+        /** Lambda function that is responsible for invalidating the cache of the CloudFront distribution. */
+        const invalidateFunction = new lambda.Function(this, 'InvalidateCacheFunction', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'index.handler',
+            code: lambda.Code.fromInline(`
+                const aws = require('aws-sdk');
+                const cloudfront = new aws.CloudFront();
+            
+                exports.handler = async (event) => {
+                    const invalidationParams = {
+                    DistributionId: '${cloudfrontDistribution.distributionId}',
+                    InvalidationBatch: {
+                        CallerReference: new Date().toString(),
+                        Paths: {
+                        Quantity: 1,
+                        Items: ['/*'], // Invalida todos los archivos en la distribución
+                        },
+                    },
+                    };
+            
+                    await cloudfront.createInvalidation(invalidationParams).promise();
+                };
+            `),
+        });
+
+        // Adds lambda function as a target to the s3 event
+        s3EventRule.addTarget(new targets.LambdaFunction(invalidateFunction));
 
         // Displays Website domain name on CloudFormation output
         new cdk.CfnOutput(this, 'CloudFrontDomainName', {
