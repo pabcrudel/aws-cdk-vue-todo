27779ca13011278033aa796eb111dfddefbd4988
pabcrudel <pabcrudel@gmail.com>
2023-08-07 19:40:54 +0200

    Updated comments

--- cdk/lib/constructs/todo-manager.ts
+++ cdk/lib/constructs/todo-manager.ts
@@ -3,7 +3,6 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-// import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 let tableName: string;
@@ -15,14 +14,7 @@ export class ToDoManagerConstruct extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        /** 
-         * DynamoDB table for storing ToDo items.
-         * 
-         * - Partition key: 'name' (String)
-         * - Sort key: 'id' (String)
-         * - Encryption: AWS-managed
-         * - Removal policy: DESTROY (table will be deleted when the stack is deleted)
-         */
+        /** DynamoDB table for storing ToDo items. */
         const todoTable = new dynamodb.Table(this, 'ToDoTable', {
             partitionKey: {
                 name: 'id',
@@ -38,29 +30,7 @@ export class ToDoManagerConstruct extends Construct {
         tableName = todoTable.tableName;
 
         /** Rest Api to communicate frontend with DynamoDB ToDo table */
-        const todoRestApi = new apigw.RestApi(this, "ToDoRestApi" 
-        // , {
-        //     defaultCorsPreflightOptions: {
-        //         allowOrigins: apigw.Cors.ALL_ORIGINS
-        //     },
-        //     policy: new iam.PolicyDocument({
-        //         statements: [
-        //             new iam.PolicyStatement({
-        //                 actions: [
-        //                     'execute-api:Invoke',
-        //                 ],
-        //                 effect: iam.Effect.ALLOW,
-        //                 principals: [
-        //                     new iam.AnyPrincipal(),
-        //                 ],
-        //                 resources: [
-        //                     'execute-api:/*',
-        //                 ],
-        //             }),
-        //         ],
-        //     }),
-        // }
-        );
+        const todoRestApi = new apigw.RestApi(this, "ToDoRestApi");
 
         // Store Api Url
         this.apiUrl = todoRestApi.url;
@@ -79,20 +49,6 @@ export class ToDoManagerConstruct extends Construct {
         todoTable.grantWriteData(putToDo);
         todoTable.grantWriteData(deleteToDo);
 
-        // const myMethodConfig: apigw.MethodOptions = {
-        //     methodResponses: [
-        //         {
-        //             statusCode: '200',
-        //             responseParameters: {
-        //                 'method.response.header.Access-Control-Allow-Headers': true,
-        //                 'method.response.header.Access-Control-Allow-Methods': true,
-        //                 'method.response.header.Access-Control-Allow-Credentials': true,
-        //                 'method.response.header.Access-Control-Allow-Origin': true,
-        //             },
-        //         },
-        //     ],
-        // };
-
         // Add the HTTP request type method to the root resource using the Lambda integration.
         todoRestApi.root.addMethod("GET", new apigw.LambdaIntegration(getAllToDos));
         todoRestApi.root.addMethod("POST", new apigw.LambdaIntegration(postToDo));
@@ -104,23 +60,16 @@ export class ToDoManagerConstruct extends Construct {
         todoRestApiChildResource.addMethod("GET", new apigw.LambdaIntegration(getToDo));
 
         /** Allow OPTIONS request without restrictions */
-        // const optionsMethods = todoRestApi.methods.filter(method => method.httpMethod === 'OPTIONS')
-        // optionsMethods.forEach(method => {
-        //     const cfnMethod = method.node.defaultChild as apigw.CfnMethod
-        //     cfnMethod.addPropertyOverride('ApiKeyRequired', false)
-        //     cfnMethod.addPropertyOverride('AuthorizationType', 'NONE')
-        //     cfnMethod.addPropertyDeletionOverride('AuthorizerId')
-        // });
+        const optionsMethods = todoRestApi.methods.filter(method => method.httpMethod === 'OPTIONS')
+        optionsMethods.forEach(method => {
+            const cfnMethod = method.node.defaultChild as apigw.CfnMethod
+            cfnMethod.addPropertyOverride('ApiKeyRequired', false)
+            cfnMethod.addPropertyOverride('AuthorizationType', 'NONE')
+            cfnMethod.addPropertyDeletionOverride('AuthorizerId')
+        });
     };
 
-    /**
-    * Configuration settings for the Lambda functions, including:
-    * 
-    * - Handler: The entry point function that Lambda executes.
-    * - Runtime: The Node.js 16.x runtime for the Lambda functions.
-    * - Environment: Environment variables to be passed to the Lambda functions.
-    *   - TABLE_NAME: The name of the DynamoDB table (`todoTable`) used by the functions.
-    */
+    /** Configuration settings for the Lambda functions. */
     private createLambdaFunction(lambdaFunctionName: string): lambdaNode.NodejsFunction {
         const formatedFunctionName = lambdaFunctionName.replace(/^./, (match) => match.toLowerCase());
 
@@ -131,16 +80,6 @@ export class ToDoManagerConstruct extends Construct {
             environment: { TABLE_NAME: tableName },
             timeout: cdk.Duration.seconds(60),
         });
-
-        // lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
-        //     effect: iam.Effect.ALLOW,
-        //     actions: [
-        //         'dynamodb:*',
-        //     ],
-        //     resources: [
-        //         '*',
-        //     ],
-        // }))
 
         return lambdaFunction;
     };
