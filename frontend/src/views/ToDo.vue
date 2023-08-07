89fe4acd176f748618bf8ea67dab1a33607848a6
pabcrudel <pabcrudel@gmail.com>
2023-08-07 20:26:10 +0200

    Created a todo view to show todos

---
+++ frontend/src/views/ToDo.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="fetchData">Obtener datos</button>
+
+    <template v-if="response.length > 0">
+        <div v-for="res in response">{{ res }}</div>
+    </template>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+import apiClient from '../../services/axios';
+
+const response = ref([]);
+
+async function fetchData() {
+    try {
+        const apiResponse = await apiClient.get();
+        response.value = apiResponse.data.items;
+    } 
+    catch (error) {
+        console.error('Error al obtener datos:', error);
+    }
+}
+</script>
+  
