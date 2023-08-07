a4532f9276d5a794a3f8d36c0a80956f2520df2d
pabcrudel <pabcrudel@gmail.com>
2023-08-07 20:25:50 +0200

    Added todo route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('../views/ToDo.vue')
     }
   ]
 })
