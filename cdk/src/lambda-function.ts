9a204f0e12017ecaef5195b8a462c366842fe86e
pabcrudel <pabcrudel@gmail.com>
2023-08-08 20:00:23 +0200

    Cors allow all methods

--- cdk/src/lambda-function.ts
+++ cdk/src/lambda-function.ts
@@ -27,7 +27,7 @@ class ApiResponse implements APIGatewayProxyResult {
         "content-type": "application/json",
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,DELETE'
     };
     readonly body: string;
 
