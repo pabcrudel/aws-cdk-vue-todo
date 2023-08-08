7a78ef6978ceb5a8102a9a4a4ce61f119c7c0da6
pabcrudel <pabcrudel@gmail.com>
2023-08-08 22:52:24 +0200

    The function varies based on parameters

--- frontend/src/components/EditToDo.vue
+++ frontend/src/components/EditToDo.vue
@@ -1,21 +1,29 @@
 <template>
-    <div>
-        <input type="text" :placeholder="props.initialText" v-model="toDoName"/>
-        <button @click="returnToDoName" v-html="'Send'"/>
+    <div class="editToDo">
+        <input type="text" :placeholder="props.initialText" v-model="toDoName" />
+        <button @click="executeFunction" v-html="'Send'" />
     </div>
 </template>
   
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const props = defineProps({initialText: String});
-
-const emit = defineEmits();
+const props = defineProps({
+    id: String,
+    date: String,
+    initialText: String,
+    sendRequest: Function
+});
 
 const toDoName = ref('');
 
-const returnToDoName = () => {
-    if (toDoName.value.length > 0) emit('ToDoName', toDoName.value);
-};
+const executeFunction = () => {
+    if (toDoName.value.length > 0 && props.sendRequest !== undefined) {
+        if (props.id !== undefined && props.date !== undefined)
+            props.sendRequest(toDoName.value);
+        else
+            props.sendRequest({ name: toDoName.value, id: props.id, date: props.date });
+    };
+}
 </script>
   
