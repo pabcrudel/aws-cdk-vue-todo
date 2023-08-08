a27bcd622f5c79abdb19e2d509acfe435552e94e
pabcrudel <pabcrudel@gmail.com>
2023-08-08 20:28:19 +0200

    Test new features

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="home">
     <h1>My ToDos</h1>
+    <div>
+        <input type="text" placeholder="Add a new ToDo" v-model="toDoName"/>
+        <button @click="createToDo(toDoName)" v-html="'Send'"/>
+    </div>
 
     <template v-if="toDos.length > 0" v-for="toDo in toDos" :key="toDo.name">
       <li v-html="toDo.name"/>
+      <button v-html="'Delete'" @click="toDoApi.deleteToDo(toDo)"/>
     </template>
-    
+
     <div v-else v-html="'ToDo table is empty'"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { toRefs, ref } from 'vue';
 import {useToDoApiStore} from '../stores/todo-api';
+import EditToDo from '@/components/EditToDo.vue';
+import type { ToDo } from '@/todo-types';
 
 const toDoApi = useToDoApiStore();
 
 const { toDos } = toRefs(toDoApi);
 
 toDoApi.getAllToDos();
+
+const createToDo = (name: string) => {
+  if (name.length > 0) {
+    toDoApi.createToDo(name);
+    toDoName.value = '';
+  };
+};
+
+const toDoName = ref('');
 </script>
