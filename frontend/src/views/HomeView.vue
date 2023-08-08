9c4a5ceaef1dcab8a070aa8e9e56385d6ccbc955
pabcrudel <pabcrudel@gmail.com>
2023-08-08 18:00:58 +0200

    ToDos will be shown in a list

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
     <h1>My ToDos</h1>
-    {{ toDoApi.toDos }}
+
+    <template v-if="toDos.length > 0" v-for="toDo in toDos" :key="toDo.name">
+      <li v-html="toDo.name"/>
+    </template>
+    
+    <div v-else v-html="'ToDo table is empty'"/>
   </div>
 </template>
 
 <script setup lang="ts">
+import { toRefs } from 'vue';
 import {useToDoApiStore} from '../stores/todo-api';
 
 const toDoApi = useToDoApiStore();
-toDoApi.getAllToDos();
 
+const { toDos } = toRefs(toDoApi);
+
+toDoApi.getAllToDos();
 </script>
