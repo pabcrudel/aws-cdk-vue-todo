c0921077f0ed3cde077dde47620247fa97161689
pabcrudel <pabcrudel@gmail.com>
2023-08-08 18:51:57 +0200

    Created a component that allows to edit ToDos

---
+++ frontend/src/components/EditToDo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <textarea v-model="toDo"></textarea>
+        <button @click="returnToDo">Send</button>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({initialText: String});
+
+const emit = defineEmits();
+
+const toDo = ref(props.initialText || '');
+
+const returnToDo = () => {
+    emit('ToDo', toDo.value);
+};
+</script>
+  
