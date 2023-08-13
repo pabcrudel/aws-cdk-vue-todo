b7d8b8961a4fbad8bfd9fa3b39fe2092229ad311
pabcrudel <pabcrudel@gmail.com>
2023-08-13 22:15:30 +0200

    Improved the form to create or edit ToDos

--- frontend/src/components/EditToDo.vue
+++ frontend/src/components/EditToDo.vue
@@ -1,24 +1,41 @@
 <template>
-    <div class="editToDo">
-        <input type="text" :placeholder="props.initialText" v-model="toDoName" />
-        <button @click="" v-html="'Send'" :disabled="disableButton"/>
-    </div>
+    <form @submit.prevent="sendRequest()" class="editToDo">
+        <fieldset>
+            <label for="todoName" v-html="'ToDo name: '" />
+            <input id="todoName" type="text" :placeholder="props.initialText" v-model="toDo.name" />
+            <button type="submit" v-html="'Send'" :disabled="disableButton" />
+        </fieldset>
+    </form>
 </template>
   
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import {ToDoPrimaryKey, ToDoAttributes} from '../todo-classes';
+import { ToDoPrimaryKey, ToDoAttributes } from '../todo-classes';
+import { useToDoApiStore } from '../stores/todo-api';
 
 const props = defineProps({
     primaryKey: ToDoPrimaryKey,
     attributes: ToDoAttributes,
-    initialText: String
+    initialText: {
+        type: String,
+        default: "Eat more vegetables",
+    },
 });
 
-const toDoName = ref('');
+const toDoApi = useToDoApiStore();
+
+const toDo = ref(props.attributes || new ToDoAttributes(''));
 
 const disableButton = computed(() => {
-    return toDoName.value.length < 1;
+    return toDo.value.name.length < 1;
 });
+
+function sendRequest() {
+    if (toDo.value.name.length > 0) {
+        if (props.primaryKey === undefined || props.attributes === undefined)
+            toDoApi.createToDo(toDo.value);
+        else toDoApi.updateToDo(props.primaryKey, toDo.value);
+    };
+};
 </script>
   
--- frontend/src/views/MyToDos.vue
+++ frontend/src/views/MyToDos.vue
@@ -1,33 +1,33 @@
 <template>
     <div class="myToDos">
-      <h1>My ToDos</h1>
-      <EditToDo :send-request="toDoApi.createToDo" :initial-text="'Add a new ToDo'" />
-  
-      <template v-if="toDos.length > 0" v-for="toDo in toDos" :key="toDo.name">
-        <EditToDo v-if="showDialog[toDo.name]" 
-          :send-request="toDoApi.updateToDo" 
-          :initial-text="toDo.name" 
-          :id="toDo.id"
-          :date="toDo.date"
-        />
-        <li v-else v-html="toDo.name" />
-        <button v-html="'Delete'" @click="toDoApi.deleteToDo(toDo)" />
-        <button v-html="'Update'" @click="showDialog[toDo.name] = !showDialog[toDo.name]" />
-      </template>
-  
-      <div v-else v-html="'You don\'t have any todo'" />
+        <h1>My ToDos</h1>
+        <EditToDo/>
+
+        <template v-if="toDos.length > 0" v-for="toDo in toDos" :key="toDo.name">
+            <EditToDo v-if="showDialog[toDo.name]" 
+                :primary-key="{id: toDo.id, date: toDo.date}"
+                :attributes="{name: toDo.name}"
+            />
+            <li v-else v-html="toDo.name" />
+            <button v-html="'Delete'" @click="toDoApi.deleteToDo(toDo)" />
+            <button v-html="'Update'" @click="showDialog[toDo.name] = !showDialog[toDo.name]" />
+        </template>
+
+        <template v-else>
+            <div v-html="'You don\'t have any todo'" />
+        </template>
     </div>
-  </template>
+</template>
   
-  <script setup lang="ts">
-  import { toRefs, ref } from 'vue';
-  import { useToDoApiStore } from '../stores/todo-api';
-  import EditToDo from '@/components/EditToDo.vue';
-  
-  const toDoApi = useToDoApiStore();
-  const { toDos } = toRefs(toDoApi);
-  toDoApi.getAllToDos();
-  
-  const showDialog = ref({} as {[key: string]: boolean;});
-  </script>
+<script setup lang="ts">
+import { toRefs, ref } from 'vue';
+import { useToDoApiStore } from '../stores/todo-api';
+import EditToDo from '@/components/EditToDo.vue';
+
+const toDoApi = useToDoApiStore();
+const { toDos } = toRefs(toDoApi);
+toDoApi.getAllToDos();
+
+const showDialog = ref({} as { [key: string]: boolean; });
+</script>
   
