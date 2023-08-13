c2aa7cbd330e90492033423f4a31947dd6257152
pabcrudel <pabcrudel@gmail.com>
2023-08-13 20:26:46 +0200

    Created some objects to manage components

--- frontend/src/components/EditToDo.vue
+++ frontend/src/components/EditToDo.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="editToDo">
         <input type="text" :placeholder="props.initialText" v-model="toDoName" />
-        <button @click="executeFunction" v-html="'Send'" :disabled="disableButton"/>
+        <button @click="" v-html="'Send'" :disabled="disableButton"/>
     </div>
 </template>
   
 <script setup lang="ts">
 import { computed, ref } from 'vue';
+import {ToDoPrimaryKey, ToDoAttributes} from '../todo-classes';
 
 const props = defineProps({
-    id: String,
-    date: String,
-    initialText: String,
-    sendRequest: Function
+    primaryKey: ToDoPrimaryKey,
+    attributes: ToDoAttributes,
+    initialText: String
 });
 
 const toDoName = ref('');
@@ -20,16 +20,5 @@ const toDoName = ref('');
 const disableButton = computed(() => {
     return toDoName.value.length < 1;
 });
-
-const executeFunction = () => {
-    if (toDoName.value.length > 0 && props.sendRequest !== undefined) {
-        if (props.id !== undefined && props.date !== undefined)
-            props.sendRequest({ name: toDoName.value, id: props.id, date: props.date });
-        else {
-            props.sendRequest(toDoName.value);
-            toDoName.value = '';
-        };
-    };
-};
 </script>
   
