2357af82d2f997cd36d9eb7d6b0d38a7c6d56c6c
pabcrudel <pabcrudel@gmail.com>
2023-08-14 17:41:07 +0200

    Only 1 form is visible when editing or adding ToDos

--- frontend/src/views/MyToDos.vue
+++ frontend/src/views/MyToDos.vue
@@ -1,33 +1,59 @@
 <template>
     <div class="myToDos">
         <h1>My ToDos</h1>
-        <EditToDo/>
 
-        <template v-if="toDos.length > 0" v-for="toDo in toDos" :key="toDo.name">
-            <EditToDo v-if="showDialog[toDo.name]" 
-                :primary-key="{id: toDo.id, date: toDo.date}"
-                :attributes="{name: toDo.name}"
-            />
-            <li v-else v-html="toDo.name" />
-            <button v-html="'Delete'" @click="toDoApi.deleteToDo(toDo)" />
-            <button v-html="showDialog[toDo.name] ? 'Close' : 'Update'" @click="showDialog[toDo.name] = !showDialog[toDo.name]" />
+        <template v-if="toDos.length > 0">
+            <EditToDo v-if="isCreating" />
+            <button @click="switchCreating" v-html="isCreating ? 'Close' : 'Create a new ToDo'" />
+
+            <template v-for="toDo in toDos" :key="toDo.name">
+                <EditToDo v-if="isEditing(toDo.id)" 
+                    :primary-key="new ToDoPrimaryKey(toDo.id, toDo.date)"
+                    :attributes="new ToDoAttributes(toDo.name)" />
+                <li v-else v-html="toDo.name" />
+
+                <button @click="toDoApi.deleteToDo(toDo)" v-html="'Delete'" />
+                <button @click="switchEditing(toDo.id)" v-html="isEditing(toDo.id) ? 'Close' : 'Update'" />
+            </template>
         </template>
 
         <template v-else>
             <div v-html="'You don\'t have any todo'" />
+            <EditToDo v-if="isCreating" />
+            <button @click="switchCreating" v-html="isCreating ? 'Close' : 'Try adding a new one'" />
         </template>
     </div>
 </template>
   
 <script setup lang="ts">
-import { toRefs, ref } from 'vue';
+import { toRefs, ref, watch } from 'vue';
 import { useToDoApiStore } from '../stores/todo-api';
 import EditToDo from '@/components/EditToDo.vue';
+import { ToDoPrimaryKey, ToDoAttributes } from '../todo-classes';
 
 const toDoApi = useToDoApiStore();
 const { toDos } = toRefs(toDoApi);
 toDoApi.getAllToDos();
 
-const showDialog = ref({} as { [key: string]: boolean; });
+const toDoDetails = ref([] as { id: string; show: boolean; }[]);
+function isEditing(id: string): boolean { return toDoDetails.value.some(toDo => toDo.id === id && toDo.show) };
+const isCreating = ref(false);
+
+function switchCreating() {
+    isCreating.value = !isCreating.value;
+
+    toDoDetails.value.forEach(toDo => toDo.show = false);
+};
+function switchEditing(id: string) {
+    isCreating.value = false;
+
+    toDoDetails.value.forEach(toDo => toDo.show = toDo.id === id ? !toDo.show : false);
+};
+
+watch(toDoApi.$state, (state) => {
+    toDoDetails.value = state.toDos.map(toDo => {
+        return { id: toDo.id, show: false };
+    });
+});
 </script>
   
