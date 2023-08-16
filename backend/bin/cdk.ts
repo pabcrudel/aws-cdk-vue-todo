ed3310a27c8d438174137d1e3606cc7a3fa0c355
pabcrudel <pabcrudel@gmail.com>
2023-08-16 16:43:13 +0200

    Each branch can deploy the app with its name as a label

--- backend/bin/cdk.ts
+++ backend/bin/cdk.ts
@@ -3,5 +3,8 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { VueToDoStack } from '../lib/vue-todo-stack';
 
+const tag = process.env.BRANCH_NAME || '';
+const stackName =  tag + '-' + 'VueToDoStack';
+
 const app = new cdk.App();
-new VueToDoStack(app, 'VueToDoStack');
+new VueToDoStack(app, stackName);
