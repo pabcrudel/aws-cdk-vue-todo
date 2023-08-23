db5e801b19de25fac8a1ed0881d211ec906d7e52
pabcrudel <pabcrudel@gmail.com>
2023-08-23 12:55:31 +0200

    Added cognito auth to the app sync stack

--- backend/lib/appsync-stack.ts
+++ backend/lib/appsync-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CognitoAuth } from './constructs/cognito-auth';
 
 export class AppSyncStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const cognitoAuth = new CognitoAuth(this, 'CognitoAuth');
     };
 };
