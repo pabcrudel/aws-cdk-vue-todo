fd2c25bafd826dee49a43255869b0ecca52cdf68
pabcrudel <pabcrudel@gmail.com>
2023-08-23 13:31:33 +0200

    Added dynamoDB storage to the app sync stack

--- backend/lib/appsync-stack.ts
+++ backend/lib/appsync-stack.ts
@@ -1,11 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CognitoAuth } from './constructs/cognito-auth';
+import { DynamoDBStorage } from './constructs/dynamodb-storage';
 
 export class AppSyncStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const cognitoAuth = new CognitoAuth(this, 'CognitoAuth');
+
+        const dynamoDBStorage = new DynamoDBStorage(this, 'DynamoDBStorage');
     };
 };
