7fe87b0845c6b53627799a276d5fe464b2d01081
pabcrudel <pabcrudel@gmail.com>
2023-08-24 16:39:31 +0200

    Created a CRUD api resource

---
+++ backend/lib/constructs/api-resources/crud-api.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
+import { CRUDResourceProps } from './api-resource-props';
+
+export class CRUDApi extends Construct {
+    constructor(scope: Construct, id: string, props: CRUDResourceProps) {
+        super(scope, id);
+
+        /** API Gateway REST API root resource to handle CRUD functionality */
+        const crudApiRootResource = props.restApi.root.addResource('crud');
+    };
+};
