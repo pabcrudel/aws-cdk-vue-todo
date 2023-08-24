f108747d5af117585e0451f7545eb03021695e6f
pabcrudel <pabcrudel@gmail.com>
2023-08-24 19:09:27 +0200

    Added Api gateway stack to test lambdas

--- backend/bin/cdk.ts
+++ backend/bin/cdk.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { VueToDoStack } from '../lib/vue-todo-stack';
+import { ApiGatewayStack } from '../lib/api-gateway-stack';
 
 let projectName = 'VueToDoStack';
 
@@ -10,4 +11,5 @@ const tag = process.env.BRANCH_NAME;
 if (tag !== undefined) projectName =  tag + '-' + projectName;
 
 const app = new cdk.App();
-new VueToDoStack(app, projectName);
+// new VueToDoStack(app, projectName);
+new ApiGatewayStack(app, projectName);
