bb0b1fb7894ed6d57353af76744aec122072abde
pabcrudel <pabcrudel@gmail.com>
2023-08-24 12:49:04 +0200

    Added cognito props as class attributes

--- backend/lib/constructs/api-resources/auth-api.ts
+++ backend/lib/constructs/api-resources/auth-api.ts
@@ -2,18 +2,22 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import { IUserPool, IUserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 import { ApiResourceProps } from './api-resource-props';
 
 export class AuthApi extends Construct {
+    private readonly userPool: IUserPool;
+    private readonly userPoolClient: IUserPoolClient;
+
     constructor(scope: Construct, id: string, props: ApiResourceProps) {
         super(scope, id);
 
-        // Using object destructuring to extract the properties from the Construct props
-        const { restApi } = props;
+        this.userPool = props.userPool;
+        this.userPoolClient = props.userPoolClient;
 
         /** API Gateway REST API root resource to handle authentication and registration functionality */
-        const authApiRootResource = restApi.root.addResource('auth');
+        const authApiRootResource = props.restApi.root.addResource('auth');
 
         const accountResource = authApiRootResource.addResource('{id}');
 
