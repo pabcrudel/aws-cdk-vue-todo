279a41fd230971740316eac7ee74447d54dd174e
pabcrudel <pabcrudel@gmail.com>
2023-08-24 19:07:34 +0200

    Add API Gateway constructs

--- backend/lib/api-gateway-stack.ts
+++ backend/lib/api-gateway-stack.ts
@@ -1,10 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { CognitoAuth } from './constructs/cognito-auth';
+import { DynamoDBStorage } from './constructs/dynamodb-storage';
+import { AuthApi } from './constructs/api-resources/auth-api';
+import { CRUDApi } from './constructs/api-resources/crud-api';
 
 export class ApiGatewayStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const { userPool, userPoolClient } = new CognitoAuth(this, 'CognitoAuth');
+
+        const { todoTable, todoUserTableName } = new DynamoDBStorage(this, 'DynamoDBStorage');
 
         /** Rest Api to communicate frontend with the backend */
         const restApi = new apiGateway.RestApi(this, "RestApi", {
@@ -12,6 +20,20 @@ export class ApiGatewayStack extends cdk.Stack {
                 allowOrigins: apiGateway.Cors.ALL_ORIGINS,
             },
             deploy: true,
+        });
+
+        new AuthApi(this, 'AuthApiResource', {
+            restApi,
+            userPool,
+            userPoolClientID: userPoolClient.userPoolClientId
+        });
+
+        new CRUDApi(this, 'CRUDApiResource', {
+            restApi,
+            userPool,
+            userPoolClientID: userPoolClient.userPoolClientId,
+            todoTable,
+            todoUserTableName
         });
     };
 };
