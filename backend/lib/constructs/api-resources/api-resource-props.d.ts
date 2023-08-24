13e6a7f42b2b1b40cfe442d874e84e217ffbb34b
pabcrudel <pabcrudel@gmail.com>
2023-08-24 13:22:11 +0200

    Only ID is needed

--- backend/lib/constructs/api-resources/api-resource-props.d.ts
+++ backend/lib/constructs/api-resources/api-resource-props.d.ts
@@ -1,11 +1,10 @@
 import { IRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { IUserPool, IUserPoolClient } from 'aws-cdk-lib/aws-cognito';
 
 export interface ApiResourceProps {
     readonly restApi: IRestApi;
-    readonly userPool: IUserPool;
-    readonly userPoolClient: IUserPoolClient;
+    readonly userPoolID: string;
+    readonly userPoolClientID: string;
 }
 
 export interface CRUDResourceProps extends ApiResourceProps {
