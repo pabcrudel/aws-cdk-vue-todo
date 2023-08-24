bd0f12986b7c82a0e54215e1a7a82df47339c2d9
pabcrudel <pabcrudel@gmail.com>
2023-08-24 12:42:16 +0200

    Added dynamoDB table and cognito attributes as props

--- backend/lib/constructs/api-resources/api-resource-props.d.ts
+++ backend/lib/constructs/api-resources/api-resource-props.d.ts
@@ -1,5 +1,13 @@
 import { IRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { IUserPool, IUserPoolClient } from 'aws-cdk-lib/aws-cognito';
 
 export interface ApiResourceProps {
     readonly restApi: IRestApi;
+    readonly userPool: IUserPool;
+    readonly userPoolClient: IUserPoolClient;
+}
+
+export interface CRUDResourceProps extends ApiResourceProps {
+    readonly todoTable: ITable;
 }
