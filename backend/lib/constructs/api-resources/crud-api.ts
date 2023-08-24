8e12e6420e03dda5bc17d2fad67e104f26770fb1
pabcrudel <pabcrudel@gmail.com>
2023-08-24 17:27:03 +0200

    Added methods to the CRUD resource

--- backend/lib/constructs/api-resources/crud-api.ts
+++ backend/lib/constructs/api-resources/crud-api.ts
@@ -16,5 +16,38 @@ export class CRUDApi extends Construct {
         const auth = new apiGateway.CognitoUserPoolsAuthorizer(this, 'CRUDAuthorizer', {
             cognitoUserPools: [props.userPool]
         });
+
+        // Iterating over an array of method objects to create API Gateway methods for each one
+        [
+            { name: 'ListToDos', httpMethod: 'GET' },
+            { name: 'CreateToDo', httpMethod: 'PUT' },
+            { name: 'UpdateToDo', httpMethod: 'POST' },
+            { name: 'DeleteToDo', httpMethod: 'DELETE' },
+        ]
+            .forEach(resource => {
+                const lambdaFunction = new lambdaNode.NodejsFunction(
+                    this,
+                    resource.name,
+                    {
+                        entry: `./lambda-functions/crud.ts`,
+                        handler: resource.name.replace(/^./, (match) => match.toLowerCase()), // = listToDos / updateToDo,
+                        runtime: lambda.Runtime.NODEJS_16_X,
+                        environment: {
+                            TABLE_NAME: props.todoTable.tableName,
+                            SECONDARY_TABLE_NAME: props.todoUserTableName,
+                        },
+                    }
+                );
+
+                // Adding a http method, a Lambda Integration and a Cognito Authorizer
+                crudApiRootResource.addMethod(
+                    resource.httpMethod,
+                    new apiGateway.LambdaIntegration(lambdaFunction),
+                    {
+                        authorizationType: apiGateway.AuthorizationType.COGNITO,
+                        authorizer: auth
+                    }
+                );
+            });
     };
 };
