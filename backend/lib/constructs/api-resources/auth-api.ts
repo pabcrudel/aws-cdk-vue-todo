422c7db6ea30fb114a19820c674de84d7d1629b5
pabcrudel <pabcrudel@gmail.com>
2023-08-24 13:22:58 +0200

    Added a lambda integration to each child resource

--- backend/lib/constructs/api-resources/auth-api.ts
+++ backend/lib/constructs/api-resources/auth-api.ts
@@ -1,33 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
-import { IUserPool, IUserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 import { ApiResourceProps } from './api-resource-props';
 
 export class AuthApi extends Construct {
-    private readonly userPool: IUserPool;
-    private readonly userPoolClient: IUserPoolClient;
-
     constructor(scope: Construct, id: string, props: ApiResourceProps) {
         super(scope, id);
-
-        this.userPool = props.userPool;
-        this.userPoolClient = props.userPoolClient;
 
         /** API Gateway REST API root resource to handle authentication and registration functionality */
         const authApiRootResource = props.restApi.root.addResource('auth');
 
         const accountResource = authApiRootResource.addResource('{id}');
+        [
+            { name: 'register', parent: authApiRootResource },
+            { name: 'login', parent: authApiRootResource },
+            { name: 'recovery', parent: accountResource },
+            { name: 'change-password', parent: accountResource },
+            { name: 'change-username', parent: accountResource },
+            { name: 'change-email', parent: accountResource },
+        ]
+            .forEach(resource => {
+                const apiResource = resource.parent.addResource(resource.name);
 
-        const postResources = [
-            authApiRootResource.addResource('register'),
-            authApiRootResource.addResource('login'),
-            accountResource.addResource('recovery'),
-            accountResource.addResource('change-password'),
-            accountResource.addResource('change-username'),
-            accountResource.addResource('change-email'),
-        ];
+                apiResource.addCorsPreflight({
+                    allowOrigins: apiGateway.Cors.ALL_ORIGINS,
+                    allowMethods: ['POST'],
+                });
+
+                apiResource.addMethod('POST', new apiGateway.LambdaIntegration(
+                    new lambdaNode.NodejsFunction(
+                        this,
+                        resource.name.replace(/-([a-z])/g, (_, match) => match.toUpperCase()),
+                        {
+                            entry: `./lambda-functions/auth.ts`,
+                            handler: resource.name,
+                            runtime: lambda.Runtime.NODEJS_16_X,
+                            environment: {
+                                USER_POOL_ID: props.userPoolID,
+                                USER_POOL_CLIENT_ID: props.userPoolClientID,
+                            },
+                        }
+                    )
+                ));
+            });
     };
 };
