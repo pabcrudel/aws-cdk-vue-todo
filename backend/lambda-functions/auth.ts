c2658685c53ee13c0026e1a32ea1a998e099c92f
pabcrudel <pabcrudel@gmail.com>
2023-08-25 15:45:48 +0200

    Configured the region of the user pool

--- backend/lambda-functions/auth.ts
+++ backend/lambda-functions/auth.ts
@@ -1,5 +1,8 @@
+import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
 import { RequestFunction } from "./types";
 import { authResponseCors } from "./utils";
+
+const cognito = new CognitoIdentityProviderClient({ region: process.env.USER_POOL_REGION });
 
 export const register: RequestFunction = (event) => {
     return { statusCode: 200, headers: authResponseCors, body: JSON.stringify(event) };
