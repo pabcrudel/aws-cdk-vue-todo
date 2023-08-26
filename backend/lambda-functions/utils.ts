f4da14403a8c25feaec62a360f98fb8e32658307
pabcrudel <pabcrudel@gmail.com>
2023-08-26 17:34:29 +0200

    Created classes to manage api responses

--- backend/lambda-functions/utils.ts
+++ backend/lambda-functions/utils.ts
@@ -1,13 +1,4 @@
-const responseCors = {
-    "content-type": "application/json",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Origin": "*"
-};
-
-export const authResponseCors = {
-    ...responseCors,
-    "Access-Control-Allow-Methods": "POST"
-};
+import { APIGatewayProxyResult } from "aws-lambda";
 
 export class ApiError extends Error {
     statusCode: number;
@@ -26,5 +17,34 @@ export class BadRequestError extends ApiError {
 export class NotFoundError extends ApiError {
     constructor(message: string) {
         super(message, 404);
+    };
+};
+
+export class ApiResponse implements APIGatewayProxyResult {
+    readonly statusCode: number;
+    readonly headers = {
+        "content-type": "application/json",
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST'
+    };
+    readonly body: string;
+
+    constructor(statusCode: number, rawBody: any) {
+        this.statusCode = statusCode;
+        this.body = JSON.stringify(rawBody);
+    };
+};
+export class ApiSuccessResponse extends ApiResponse {
+    constructor(rawBody: any) {
+        super(200, rawBody);
+    };
+};
+export class ApiErrorResponse extends ApiResponse {
+    constructor(error: any) {
+        super(
+            error instanceof ApiError ? error.statusCode : 500,
+            { error: error instanceof Error ? error.message : "Unknown error occurred" },
+        );
     };
 };
