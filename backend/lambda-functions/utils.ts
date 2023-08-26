8c7512be111f2ce660bc38856d9f77a3e8b1ab42
pabcrudel <pabcrudel@gmail.com>
2023-08-26 17:30:31 +0200

    Created classes to manage errors

--- backend/lambda-functions/utils.ts
+++ backend/lambda-functions/utils.ts
@@ -8,3 +8,23 @@ export const authResponseCors = {
     ...responseCors,
     "Access-Control-Allow-Methods": "POST"
 };
+
+export class ApiError extends Error {
+    statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = "ApiError";
+    };
+};
+export class BadRequestError extends ApiError {
+    constructor(message: string) {
+        super(message, 400);
+    };
+};
+export class NotFoundError extends ApiError {
+    constructor(message: string) {
+        super(message, 404);
+    };
+};
